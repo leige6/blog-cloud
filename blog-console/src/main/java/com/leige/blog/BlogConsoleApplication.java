ad9cd154bb8a71cebfe21efda4456a1711beea6c
leige <yourleige@163.com>
2018-05-24 16:39:00 +0800

    更新application

--- blog-console/src/main/java/com/leige/blog/BlogConsoleApplication.java
+++ blog-console/src/main/java/com/leige/blog/BlogConsoleApplication.java
@@ -1,9 +1,13 @@
 package com.leige.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement //如果mybatis中service实现类中加入事务注解，需要此处添加该注解
+@MapperScan("com.leige.blog.mapper")//扫描的是mapper.xml中namespace指向值的包位置
 public class BlogConsoleApplication {
 
 	public static void main(String[] args) {

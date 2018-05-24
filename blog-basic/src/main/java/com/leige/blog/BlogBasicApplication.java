96daae8053b961f01af2477c7d96c852df8422d6
leige <yourleige@163.com>
2018-05-24 10:14:06 +0800

    更新application

---
+++ blog-basic/src/main/java/com/leige/blog/BlogBasicApplication.java
@@ -0,0 +1,12 @@
+package com.leige.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBasicApplication.class, args);
+	}
+}

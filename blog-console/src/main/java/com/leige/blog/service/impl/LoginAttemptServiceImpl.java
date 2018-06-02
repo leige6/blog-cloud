4546561a6ce67977f90e4fd6118569cfad2f5d0b
leige6 <yourleige@gmail.com>
2018-06-03 02:07:14 +0800

    更新

--- blog-console/src/main/java/com/leige/blog/service/impl/LoginAttemptServiceImpl.java
+++ blog-console/src/main/java/com/leige/blog/service/impl/LoginAttemptServiceImpl.java
@@ -3,6 +3,7 @@ package com.leige.blog.service.impl;
 import com.leige.blog.common.utils.RedisUtil;
 import com.leige.blog.service.LoginAttemptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -12,7 +13,10 @@ import java.util.concurrent.TimeUnit;
  */
 @Service
 public class LoginAttemptServiceImpl  implements LoginAttemptService {
-    private final int MAX_ATTEMPT = 3;
+    @Value("${login.fail.maxattempt}")
+    private  int MAX_ATTEMPT;
+    @Value("${login.fail.locktime}")
+    private  long LOCK_TIME;
     private static  final String failNameHead="loginFailCount:";
     @Autowired
     private RedisUtil redisUtil;
@@ -27,7 +31,7 @@ public class LoginAttemptServiceImpl  implements LoginAttemptService {
 
     public void loginFailed(String key) {
         redisUtil.increment(failNameHead+key,1L);
-        redisUtil.expire(failNameHead+key,60L, TimeUnit.SECONDS);
+        redisUtil.expire(failNameHead+key,LOCK_TIME, TimeUnit.SECONDS);
     }
 
     public boolean isBlocked(String key) {
